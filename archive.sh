5892d5ff0f0b67378a5abe955f37a5178ca906e9
Toby Youngberg <tyoungberg@sofi.org>
2019-08-30 13:23:42 -0600

    Fixed up some stuff

---
+++ archive.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Delete the existing archives or you can't create new ones
+rm -rf build
+
+# Archive a framework for each architecture.  Be sure that BUILD_LIBRARIES_FOR_DISTRIBUTION is yes or binary compatibility won't work.
+xcodebuild archive -scheme Utils -destination="iOS" -archivePath build/ios.xcarchive -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme Utils -destination="iOS Simulator" -archivePath build/iossimulator.xcarchive -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+# Bundle together a new xcframework with the archives for each architecture
+xcodebuild -create-xcframework -framework build/ios.xcarchive/Products/Library/Frameworks/Utils.framework -framework build/iossimulator.xcarchive/Products/Library/Frameworks/Utils.framework -output build/Utils.xcframework
+
+# Get rid of the intermediary xcarchive files
+rm -rf build/*.xcarchive
+
+zip -r Utils.xcarchive.zip build
+
+rm -rf build
+
+# Open the directory of the finished archive
+open .
