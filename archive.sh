558724c4a9ddd06bebebf4af7b595e1128761264
Toby Youngberg <tyoungberg@sofi.org>
2019-08-30 14:20:09 -0600

    Fixed a build setting and updated xcarchive script.

--- archive.sh
+++ archive.sh
@@ -8,12 +8,16 @@ xcodebuild archive -scheme Utils -destination="iOS" -archivePath build/ios.xcarc
 xcodebuild archive -scheme Utils -destination="iOS Simulator" -archivePath build/iossimulator.xcarchive -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
 # Bundle together a new xcframework with the archives for each architecture
-xcodebuild -create-xcframework -framework build/ios.xcarchive/Products/Library/Frameworks/Utils.framework -framework build/iossimulator.xcarchive/Products/Library/Frameworks/Utils.framework -output build/Utils.xcframework
+xcodebuild -create-xcframework -framework build/ios.xcarchive/Products/Library/Frameworks/Utils.framework -framework build/iossimulator.xcarchive/Products/Library/Frameworks/Utils.framework -output build/Carthage/Build/iOS/Utils.xcframework
 
 # Get rid of the intermediary xcarchive files
 rm -rf build/*.xcarchive
 
-zip -r Utils.xcarchive.zip build
+cd build
+
+zip -r ../Utils.xcframework.zip Carthage/*
+
+cd ..
 
 rm -rf build
 
